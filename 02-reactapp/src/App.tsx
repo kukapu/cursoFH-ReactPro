18a9a499742c98650916e25024d41370d3b22fbf
Albert Alvarez Estelles <susokukapu@gmail.com>
2023-02-17 19:11:10 +0100

    3 build initial project ReactDom

---
+++ 02-reactapp/src/App.tsx
@@ -0,0 +1,12 @@
+import { Navigation } from "./router/NAvigation"
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
---
+++ 02-reactapp/src/router/Navigation.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, NavLink } from "react-router-dom"
+import logo from '../assets/react.svg'
+import { Routes, Route, Navigate } from 'react-router';
+import '../index.css'
+
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={ logo } alt="logo" />
+          <ul>
+            <li>
+              <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/home" element={ <h1> HOME </h1> } />
+          <Route path="/about" element={ <h1> ABOUT PAGE </h1> } />
+          <Route path="/users" element={ <h1> USER PAGE </h1> } />
+
+          <Route path="*" element={ <Navigate to='/home' replace /> }/>
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  )
+}
